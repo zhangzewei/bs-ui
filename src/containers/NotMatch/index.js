46190ab5958671ca839bc34f0b017d2ba07f8db3
zhanzgewei <453646474@qq.com>
2018-05-07 15:15:46 +0800

    feat: 增加404页面

--- src/containers/NotMatch/index.js
+++ src/containers/NotMatch/index.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import Page404 from './images/404.png';
+import './style.css';
 
-class NotMatch extends Component {
-  static propTypes = {
-  }
-
+export default class NotMatch extends Component {
   render() {
     return (
-      <div>
-        404
+      <div className="page-404">
+        <img alt="页面找不到了" src={Page404} />
       </div>
     );
   }
 }
-
-export default NotMatch;
